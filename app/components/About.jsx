8f15687282c123881264381c1e99ea9a1ba4d9d0
Aroon1 <hojski@gmx.at>
2017-04-07 17:43:17 +0200

    Style About page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,18 +1,26 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>About Component</h3>
-//     )
-//   }
-// });
-
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About Page!</p>
+      <h1 className="text-center">About</h1>
+      <p>This is Weather Application build on React. The purpose
+        of this programm is to enable quick Search querys for
+        your input and rendering the weather of the searched location.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This is
+            the used Javascript framework.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - This
+            is the API used for fetching the weather data.
+        </li>
+      </ul>
     </div>
   )
 };
